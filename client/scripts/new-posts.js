85dea798d0cb5c8e98677a65b920b229fecb863e
Mark <mark.shark05@hotmail.com>
2017-08-26 15:58:28 +0300

    Fix recent posts preview

--- client/scripts/new-posts.js
+++ client/scripts/new-posts.js
@@ -8,21 +8,21 @@ $(function() {
         }
         function updateStorage(i) {
             var saved_posts_count = JSON.parse(localStorage.posts_count);
-            saved_posts_count[i] = current_posts_count[i];
+            saved_posts_count[i] = window.ctx.current_posts_count[i];
             localStorage.posts_count = JSON.stringify(saved_posts_count);
         }
 
         if (localStorage.posts_count) {
             var saved_posts_count = JSON.parse(localStorage.posts_count);
             $('.thumb').each(function(i, thumb) {
-                if (current_posts_count[i] > saved_posts_count[i]) {
+                if (window.ctx.current_posts_count[i] > saved_posts_count[i]) {
                     highlight(thumb);
-                } else if (current_posts_count[i] < saved_posts_count[i]) {
+                } else if (window.ctx.current_posts_count[i] < saved_posts_count[i]) {
                     updateStorage(i);
                 }
             });
         } else {
-            localStorage.posts_count = JSON.stringify(current_posts_count);
+            localStorage.posts_count = JSON.stringify(window.ctx.current_posts_count);
         }
 
         $('.thumb').on('click', function() {
--- server/router.js
+++ server/router.js
@@ -1,7 +1,13 @@
 function init(app, data) {
     app.get('/', (req, res) => {
+        const users = data.users.getAll();
+        const postsCount = users.map((u) => u.media.count);
+
         res.render('home', {
-            users: data.users.getAll(),
+            users: users,
+            ctx: {
+                current_posts_count: postsCount,
+            },
         });
     });
 }
